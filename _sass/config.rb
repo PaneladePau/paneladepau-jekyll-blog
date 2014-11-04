d268fc7311fc9546b6558bee33f612e10402eb6a
Guilherme <guilherme.moreira@inkuba.com.br>
2014-11-04 01:17:23 -0200

    minificacao de css configurada no compass

--- _sass/config.rb
+++ _sass/config.rb
@@ -6,16 +6,15 @@ sass_dir = "./"
 css_dir = "../css"
 images_dir = "../img"
 javascripts_dir = "../js"
-relative_assets = true
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded #or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
